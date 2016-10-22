404aa7e00509d08f7de9f65d4d6dc34c96531a97
Collin Bartley <boloman94@gmail.com>
2016-10-22 01:21:56 -0400

    The first start of a website

---
+++ index.php
@@ -0,0 +1,25 @@
+<!?php phpinfo(); ?>
+
+
+<html>
+<head>
+<title>Test</title>
+</head>
+
+<body>
+<h1>
+	HELLO
+</h1>
+
+Try this out!!!
+Use w3schools to make a website that will connect to your database remotely.
+If you can get that working then you should go ahead and try to maipulate the data insde your database.  
+
+THen you should make the site look pretty .
+
+WHile you are doing this, you should add it all to github so that you can have emil and possibly joe check it out for you.
+
+
+</bodyu>
+
+</html>
