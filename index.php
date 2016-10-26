10801a6be509fa0fcbe2ff3270c43ee29b7f0f7c
Collin Bartley <boloman94@gmail.com>
2016-10-25 23:01:06 -0400

    Changed the paragraph in the thml portion

--- index.php
+++ index.php
@@ -11,14 +11,10 @@
 	HELLO
 </h1>
 
-Try this out!!!
-Use w3schools to make a website that will connect to your database remotely.
-If you can get that working then you should go ahead and try to maipulate the data insde your database.  
-
-THen you should make the site look pretty .
-
-WHile you are doing this, you should add it all to github so that you can have emil and possibly joe check it out for you.
-
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+  Name: <input type="text" name="fname">
+  <input type="submit">
+</form>
 
 </body>
 
